11a40d39b0b3170f7a2652b4fe7f01d35c00fafe
Ali Hallaji <ali.hallaji1@gmail.com>
2014-03-15 13:17:57 +0330

    Update MongoDriver.py

--- MongoDriver.py
+++ MongoDriver.py
@@ -2,35 +2,31 @@ from pymongo import MongoClient, ASCENDING, Connection, DESCENDING
 from datetime import datetime, timedelta
 from twisted.web import xmlrpc, server
 from twisted.python import log, failure
-from twisted.internet import pollreactor
-from twisted.internet.defer import Deferred
-from twisted.internet.task import deferLater
-
+from twisted.internet.threads import deferToThread
 
 import csv
 import time
 import os
 log.startLogging(open('mongolog', 'w'), setStdout=False)
 
-class MongoService(xmlrpc.XMLRPC):
+class MongoTest(xmlrpc.XMLRPC):
 	
 	"""
-		Note: * is meaning that pass two args to def for name_db and name_col
-		for connecting:
+		for client connecting:
 		>>>from xmlrpclib import ServerProxy
 		>>>s = ServerProxy("http://localhost:7081")
-		>>>s.count(val) # pass arg by dict for finding and matching		*
-		>>>s.remove(val) # pass arg by dict for finding and matching  	*
-		>>>s.fetchOne(val) # pass arg by dict for finding and matching		*
-		>>>s.DropDB(name) # Drop your database. name=name of db 	*
-		>>>s.ensureIndex(name, num) # set ensureIndex, name=key & num=(ASCENDING(1) or DESCENDING(-1))	*
-		>>>s.fetchAll(val) # pass arg by dict for finding 	*
-		>>>s.update({},{}) # pass two dict of args. dict1 = find keys, dict2 = set update keys 	*
-		>>>s.ImportByMongo() # import data csv or json file by mongo import command
-		>>>s.InsertToMongo() # Single insert
-		>>>s.BulkToMongo(number) # for bulking insert, number is chunk of data 	*
-		>>>s.indexBulk(number) # for bulking insert with indexing, number is chunk of data 	*
-		>>>s.addRecord(val) # pass the your dict 	*
+		>>>s.asyncCounte(name_db, name_col, val) # pass arg by dict for finding and matching
+		>>>s.asyncRemove(name_db, name_col, val) # pass arg by dict for finding and matching 
+		>>>s.asyncfetchOne(name_db, name_col, val) # pass arg by dict for finding and matching
+		>>>s.DropDB(name) # Drop your database. name=name of db 
+		>>>s.asyncEnsureIndex(name_db, name_col, key, num) # set ensureIndex, name=key & num=(ASCENDING(1) or DESCENDING(-1))
+		>>>s.asyncfetchAll(name_db, name_col, val) # pass arg by dict for finding 
+		>>>s.asyncUpdate(name_db, name_col, val, VAL) # pass two dict of args. dict1 = find keys, dict2 = set update keys
+		>>>s.ImportByMongo(type) # import data csv or json file by mongo import command. type='csv' or 'json'
+		>>>s.asyncInsert(name_db, name_col, number) # Single inserting
+		>>>s.asyncBulk(name_db, name_col, number) # for bulking insert, number is chunk of data 	
+		>>>s.asyncIndexBulking(name_db, name_col, number)
+		>>>s.asyncAddRecord(name_db, name_col, val)
 		>>>s.stop() # kill server
 	"""
 	log.msg("Start the module")
@@ -42,24 +38,20 @@ class MongoService(xmlrpc.XMLRPC):
 		self.dir = '/home/pythonu/Desktop/check.csv'
 		self.dir_json = '/home/pythonu/Desktop/check.json'
 		self.Dict = {}
-		reactor.suggestThreadPoolSize(30)
 
 
 	def xmlrpc_getInfo(self):
-
-		return MongoService.__doc__
+		return MongoTest.__doc__
 
 	def GetTime(self, secs):
 		"""
 			this is convert function from secs to strftime format
 			pass your secs to this func for converting to strftime("%H:%M:%S")
 		"""
-
 		c = ":"
 		sec = timedelta(seconds=int(secs))
 		d = datetime(1,1,1) + sec
 		val = "%s:%s:%s" % (d.hour, d.minute, d.second)
-
 		# This line for add zero and check standard time
 		return c.join("{0:02d}".format(int(i)) for i in val.split(c))
 
@@ -70,13 +62,17 @@ class MongoService(xmlrpc.XMLRPC):
 		"""
 		print ">>> Call DropDB"
 		client = MongoClient()
-		c = Connection()
-		c.drop_database(str(name))
 		all_db = client.database_names()
-		if str(name) in all_db:
+		for i in all_db:
+			if str(name) == str(i):
+				c = Connection()
+				c.drop_database(str(name))
+				log.msg("Droped your DB")
+				return "Droped your DB"
+		else:
+			log.msg("Faild to Drop your db")
 			return "Faild to Drop your db"
-		log.msg("Droped your DB")
-		return "Droped your DB"
+
 		# c['mydatabase'].drop_collection('mycollection') ; for collection
 
 	def xmlrpc_BulkToMongo(self, name_db, name_col, number):
@@ -106,6 +102,15 @@ class MongoService(xmlrpc.XMLRPC):
 		print "\r%s records added by Bulking in my db after %s time\n " % (i,now_time)
 		return "bulking %s records in %s time" % (i, now_time)
 
+
+	def xmlrpc_asyncBulk(self, name_db, name_col, number):
+		"""
+			this is for async call function
+		"""
+		print ">>>call Bulking to Mongo"
+		d = deferToThread(self.xmlrpc_BulkToMongo, name_db, name_col, number)
+		return d
+
 	def xmlrpc_getDict(self):
 		"""
 			Imprest Dict
@@ -130,6 +135,16 @@ class MongoService(xmlrpc.XMLRPC):
 					now = time.time() - self.start
 					now_time = self.GetTime(now)
 					print "\r%s records added by inserting in my db after %s time\n " % (i,now_time)
+		print "\r%s records added by inserting in my db after %s time\n " % (i,now_time)
+		return "inserting %s records in %s time" % (i, now_time)
+
+	def xmlrpc_asyncInsert(self, name_db, name_col, number):
+		"""
+			this is for async call function
+		"""
+		print ">>>call Inserting to Mongo"
+		d = deferToThread(self.xmlrpc_InsertToMongo, name_db, name_col, number)
+		return d
 
 	def xmlrpc_ImportByMongo(self, type):
 		"""
@@ -157,6 +172,14 @@ class MongoService(xmlrpc.XMLRPC):
 		now = time.time() - self.start
 		return str(seek), self.GetTime(now)
 
+	def xmlrpc_asyncfetchOne(self, name_db, name_col, val):
+		"""
+			this is for async call function
+		"""
+		print ">>>call fetch one from Mongo"
+		d = deferToThread(self.xmlrpc_fetchOne, name_db, name_col, val)
+		return d
+
 	def xmlrpc_fetchAll(self, name_db, name_col, val):
 		"""
 			fetch all records when match keys
@@ -175,6 +198,14 @@ class MongoService(xmlrpc.XMLRPC):
 		now = time.time() - self.start
 		return tp, i, self.GetTime(now)
 
+	def xmlrpc_asyncfetchAll(self, name_db, name_col, val):
+		"""
+			this is for async call function
+		"""
+		print ">>>call fetch All from Mongo"
+		d = deferToThread(self.xmlrpc_fetchAll, name_db, name_col, val)
+		return d
+
 	def xmlrpc_update(self, name_db, name_col, val, VAL):
 		"""
 			update records
@@ -191,6 +222,14 @@ class MongoService(xmlrpc.XMLRPC):
 		now = time.time() - self.start
 		return self.GetTime(now)
 
+	def xmlrpc_asyncUpdate(self, name_db, name_col, val, VAL):
+		"""
+			this is for async call function
+		"""
+		print ">>>call update record from Mongo"
+		d = deferToThread(self.xmlrpc_update, name_db, name_col, val, VAL)
+		return d
+
 	def xmlrpc_remove(self, name_db, name_col, val):
 		"""
 			delete records
@@ -203,6 +242,14 @@ class MongoService(xmlrpc.XMLRPC):
 		db.db_col.remove(val)
 		now = time.time() - self.start
 		return self.GetTime(now)
+
+	def xmlrpc_asyncRemove(self, name_db, name_col, val):
+		"""
+			this is for async call function
+		"""
+		print ">>>call remove record from Mongo"
+		d = deferToThread(self.xmlrpc_remove, name_db, name_col, val)
+		return d
 
 	def xmlrpc_count(self, name_db, name_col, val):
 		"""
@@ -218,13 +265,33 @@ class MongoService(xmlrpc.XMLRPC):
 		now = time.time() - self.start
 		return key, self.GetTime(now)
 
-	def xmlrpc_ensureIndex(self, name_db, name_col, name, num):
+	def xmlrpc_asyncCounte(self, name_db, name_col, val):
+		"""
+			this is for async call function
+		"""
+		print ">>>call count records from Mongo"
+		d = deferToThread(self.xmlrpc_count, name_db, name_col, val)
+		return d
+
+
+	def xmlrpc_ensureIndex(self, name_db, name_col, key, num):
 		print ">>> Call ensureIndex"
+		self.start = time.time()
 		client = MongoClient()
 		db = client[str(name_db)]
 		db_col = db[str(name_col)]
-		db.db_col.ensure_index([(str(name), int(num))])
-		return "set index"
+		db.db_col.ensure_index([(str(key), int(num))])
+		now = time.time() - self.start
+		return "set indexed in %s time" % self.GetTime(now)
+
+	def xmlrpc_asyncEnsureIndex(self, name_db, name_col, key, num):
+		"""
+			this is for async call function
+		"""
+		print ">>>call ensureIndex to Mongo"
+		d = deferToThread(self.xmlrpc_ensureIndex, name_db, name_col, key, num)
+		return d
+
 
 	def xmlrpc_indexBulk(self, name_db, name_col, number):
 		"""
@@ -258,12 +325,20 @@ class MongoService(xmlrpc.XMLRPC):
 		print "\r%s records added by Bulking in my db after %s time\n " % (i,now_time)
 		return "bulking %s records in %s time" % (i, now_time)
 
+	def xmlrpc_asyncIndexBulking(self, name_db, name_col, number):
+		"""
+			this is for async call function
+		"""
+		print ">>>call indexBulk to Mongo"
+		d = deferToThread(self.xmlrpc_indexBulk, name_db, name_col, number)
+		return d
+
 	def xmlrpc_addRecord(self, name_db, name_col, val):
 		"""
 			add record
 			val: pass dict of keys
 		"""
-		print ">>> Call ensureIndex"
+		print ">>> Call AddRecord"
 		self.start = time.time()
 		client = MongoClient()
 		db = client[str(name_db)]
@@ -273,6 +348,13 @@ class MongoService(xmlrpc.XMLRPC):
 		now_time = self.GetTime(now)
 		return "added your record in %s time" % now_time
 
+	def xmlrpc_asyncAddRecord(self, name_db, name_col, val):
+		"""
+			this is for async call function
+		"""
+		print ">>>call indexBulk to Mongo"
+		d = deferToThread(self.xmlrpc_addRecord, name_db, name_col, number)
+		return d
 
 	def xmlrpc_stop(self):
 		"""
@@ -283,15 +365,14 @@ class MongoService(xmlrpc.XMLRPC):
 	def xmlrpc_fault(self):
 		f = failure.Failure()
 		log.err(f, "error")
-		raise xmlrpc.Fault(123, "Kharabi!")
+		raise xmlrpc.Fault(123, "Error!")
 
 
 
 if __name__ == "__main__":
 	from twisted.internet import reactor
-	r = MongoService()
+	r = MongoTest()
 	reactor.listenTCP(7081, server.Site(r))
 	observer = log.PythonLoggingObserver()
-	#log.err(f, "sdsd")
 	observer.start()
 	reactor.run()
